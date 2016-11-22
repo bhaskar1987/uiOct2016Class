988899baa1843258ba46efd3793a231f533eb184
bhaskar reddy yasani <bhaskar reddy yasani>
2016-11-22 08:57:16 -0800

    javascript primitive datatypes assignment

---
+++ javascript/11_22_2016/bhaskar/datatypeprimitive.js
@@ -0,0 +1,60 @@
+/*
+    this is basic javascript file which deals with primitive data types
+    namely numbers, strings,boolean,null and undefined
+*/
+
+
+// number data type
+
+var x = 30;
+var y = 50;
+
+// string data type
+
+var name = 'bhaskar';
+var street = 'ramalaym veedi';
+
+// boolean data type
+
+var isSelected = true;
+var isFinished = false;
+
+// null data type
+
+var p = null;
+var employer = null;
+
+// undefined data type
+
+var q = undefined;
+var salary = undefined;
+
+// updating some variable values to see the changes
+
+y = 75;
+street = 'behind the head post office';
+employer = 'Apple';
+salary = 3000;
+
+
+//printing values inside console
+
+console.log(x);
+console.log(y);
+console.log(name);
+console.log(street);
+console.log(isSelected);
+console.log(isFinished);
+console.log(p);
+console.log(employer);
+console.log(q);
+console.log(salary);
+
+// consoling variable which is not declared to see compilation/execution error, further execution won't happen.
+
+console.log(m);
+
+
+
+
+
